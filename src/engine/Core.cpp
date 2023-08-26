6baedfc4f76772e50814454b2911097535d36465
Blazer60 <ryanpurse@gmail.com>
2023-08-26 10:43:47 +0100

    Changed the default colours to green.

--- src/engine/Core.cpp
+++ src/engine/Core.cpp
@@ -119,9 +119,51 @@ bool engine::Core::initImGui()
     ImGuiStyle &style = ImGui::GetStyle();
     if (mGuiIo->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
-        style.WindowRounding = 0.f;
+        style.WindowRounding = 5.f;
         style.Colors[ImGuiCol_WindowBg].w = 1.f;
     }
+    
+    style.FrameRounding = 5.f;
+    style.GrabRounding = 5.f;
+    
+    style.WindowBorderSize = 0.f;
+    style.ChildBorderSize = 0.f;
+    style.PopupBorderSize = 0.f;
+    
+    style.Colors[ImGuiCol_Text] = ImVec4(0.85f, 0.85f, 0.85f, 1.f);
+    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.16f, 0.16f, 0.17f, 1.f);
+    style.Colors[ImGuiCol_ChildBg] = ImVec4(0.16f, 0.16f, 0.17f, 1.f);
+    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.2f, 0.2f, 0.2f, 1.f);
+    
+    style.Colors[ImGuiCol_Header] = ImVec4(0.053f, 0.509f, 0.f, 0.31f);
+    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.053f, 0.509f, 0.f, 0.8f);
+    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.053f, 0.509f, 0.f, 1.f);
+    
+    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.053f, 0.509f, 0.f, 0.31f);
+    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.053f, 0.509f, 0.f, 0.8f);
+    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.053f, 0.509f, 0.f, 1.f);
+    
+    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.2f, 0.2f, 0.2f, 1.f);
+    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.2f, 0.2f, 0.2f, 1.f);
+    
+    style.Colors[ImGuiCol_CheckMark]            = ImVec4(0.053f, 0.6f, 0.f, 1.f);
+    style.Colors[ImGuiCol_SliderGrab]           = ImVec4(0.053f, 0.6f, 0.f, 0.9f);
+    style.Colors[ImGuiCol_SliderGrabActive]     = ImVec4(0.053f, 0.6f, 0.f, 1.f);
+    style.Colors[ImGuiCol_Button]               = ImVec4(0.053f, 0.6f, 0.f, 0.4f);
+    style.Colors[ImGuiCol_ButtonHovered]        = ImVec4(0.053f, 0.6f, 0.f, 0.9f);
+    style.Colors[ImGuiCol_ButtonActive]         = ImVec4(0.053f, 0.6f, 0.f, 1.f);
+    
+    style.Colors[ImGuiCol_Separator] = ImVec4(0.16f, 0.16f, 0.17f, 0.9f);
+    style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.053f, 0.6f, 0.f, 1.f);
+    style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.053f, 0.6f, 0.f, 1.f);
+    
+    style.Colors[ImGuiCol_Tab]          = ImVec4(0.053f, 0.509f, 0.f, 0.5f);
+    style.Colors[ImGuiCol_TabHovered]   = ImVec4(0.053f, 0.509f, 0.f, 0.8f);
+    style.Colors[ImGuiCol_TabActive]    = ImVec4(0.053f, 0.509f, 0.f, 1.f);
+    style.Colors[ImGuiCol_TabUnfocused] = ImVec4(0.053f, 0.509f, 0.f, 0.5f);
+    style.Colors[ImGuiCol_TabUnfocusedActive]    = ImVec4(0.053f, 0.509f, 0.f, 0.6f);
+    
+    style.Colors[ImGuiCol_DockingPreview] = ImVec4(0.8f, 0.8f, 0.8f, 0.5f);
     
     if (!ImGui_ImplGlfw_InitForOpenGL(mWindow, true))
         return false;
